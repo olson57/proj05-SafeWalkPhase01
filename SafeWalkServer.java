2cd29899b5938459c2ed2943df6a3e6bfdb1b3be
olson57 <olson57@purdue.edu>
2014-11-08 16:10:49 -0500

    Added Skeleton for main and two constructors

--- SafeWalkServer.java
+++ SafeWalkServer.java
@@ -1,5 +1,18 @@
 import java.net.ServerSocket;
 
 public class SafeWalkServer {
-	
+	public final String[] LOCS = {"CL50", "EE", "LWSN", 
+                                  "PMU", "PUSH", "*"};
+
+    public SafeWalkServer(int port) {
+
+    }
+
+    public SafeWalkServer() {
+
+    }
+
+    public static void main(String[] args) {
+    	
+    }		
 }
