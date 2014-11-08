92cd17bb4b77a635d78eebc7ecc0ab994030da18
olson57 <olson57@purdue.edu>
2014-11-08 16:40:19 -0500

    Added thread to server. added default no argument commandline input to main.

--- SafeWalkServer.java
+++ SafeWalkServer.java
@@ -1,18 +1,23 @@
 import java.io.*;
 import java.net.*;
 
-public class SafeWalkServer {
+public class SafeWalkServer implements Runnable {
 	public final String[] LOCS = {"CL50", "EE", "LWSN", 
                                   "PMU", "PUSH", "*"};
     private int port;
+    private ServerSocket socket;
     public static final int DEFAULT_PORT = 4242;
 
     public SafeWalkServer(int port) throws SocketException, IOException {
+    	if (isValidPort(port)) {
+    		ServerSocket s = new ServerSocket(port);
+    	} else {
 
+    	}
     }
 
     public SafeWalkServer() throws SocketException, IOException{
-
+    	ServerSocket s = new ServerSocket(DEFAULT_PORT);
     }
 
     public int getLocalPort() {
@@ -25,7 +30,15 @@ public class SafeWalkServer {
     	}
     }
 
-    public boolean isValidInput(String input) {
+    private boolean isValidInput(String input) {
+
+    	return false;
+    }
+
+    private boolean isValidPort(int port) {
+    	if (port > 1025 && port < 65535) {
+    		return true;
+    	}
 
     	return false;
     }
@@ -33,6 +46,8 @@ public class SafeWalkServer {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
     	if (args.length == 0) {
     		System.out.printf("Port not specified. Using free port %d", DEFAULT_PORT);
+    		SafeWalkServer s = new SafeWalkServer();
+    		s.run();
     	}
     }		
 }
