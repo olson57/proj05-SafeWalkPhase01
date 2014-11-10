955e5eaa0d49f626f3c44ee6ca4bccfa6b5a1da5
olson57 <olson57@purdue.edu>
2014-11-10 14:39:02 -0500

    Added try catch in main and CLI parsing

--- SafeWalkServer.java
+++ SafeWalkServer.java
@@ -3,7 +3,7 @@ import java.net.*;
 import java.util.ArrayList;
 
 public class SafeWalkServer implements Runnable {
-	public final String[] LOCS = {"CL50", "EE", "LWSN", 
+    public final String[] LOCS = {"CL50", "EE", "LWSN", 
                                   "PMU", "PUSH", "*"};
     private int port;
     private ServerSocket socket;
@@ -11,93 +11,101 @@ public class SafeWalkServer implements Runnable {
     private ArrayList<Socket> clients;
 
     public SafeWalkServer(int port) throws SocketException, IOException {
-    	if (isValidPort(port)) {
-    		socket = new ServerSocket(port);
-    		clients = new ArrayList<Socket>();
-    	} else {
-    		System.out.printf("Invalid port.");
-    	}
+        if (isValidPort(port)) {
+            socket = new ServerSocket(port);
+            clients = new ArrayList<Socket>();
+        } else {
+            System.out.printf("Invalid port.");
+        }
     }
 
     public SafeWalkServer() throws SocketException, IOException{
-    	socket = new ServerSocket(DEFAULT_PORT); 
-    	clients = new ArrayList<Socket>();
+        socket = new ServerSocket(DEFAULT_PORT); 
+        clients = new ArrayList<Socket>();
     }
 
     public int getLocalPort() {
-    	return port;
+        return port;
     }
 
     public synchronized void pairClients(Socket s) {
-    	for (int i = 0; i < clients.size(); i++) {
-    		if (sameStartingLocation(clients.get(i), s)) {
-    			//check destination.
-    		}
-    	}
+        for (int i = 0; i < clients.size(); i++) {
+            if (sameStartingLocation(clients.get(i), s)) {
+                //check destination.
+            }
+        }
     }
 
     private boolean sameStartingLocation(Socket client1, Socket client2) {
-    	return false;
+        return false;
     }
 
     public void run() {
-    	while (true) {
-    		try {
-    			Socket client = socket.accept();
-    			clients.add(client);
-    			pairClients(client);
-    		} catch (IOException e) {
-    			e.printStackTrace();
-    		}
-    	}
+        while (true) {
+            try {
+                Socket client = socket.accept();
+                clients.add(client);
+                pairClients(client);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private boolean isValidInput(String input) {
-    	String temp = input;
-    	int commaIndex = 0;
-    	int previousIndex = 0;
-    	int partsIndex = 0;
-    	String[] parts = new String[4];
-    	boolean validFrom = false;
-    	boolean validTo = false;
-
-    	for (int i = 0; i < (temp.length() - 1); i++) {
-    		commaIndex = i;
-    		if (temp.charAt(i) == ',') {
-    			parts[partsIndex] = temp.substring(previousIndex, commaIndex);
-    			previousIndex = ++commaIndex;
-    			partsIndex++;
-    		}
-    	}
-
-    	for (int i = 0; i < (LOCS.length - 1); i++) {
-    		if (parts[1] == LOCS[i]) {
-    			validFrom = true;
-    		}
-    	}
-
-    	for (int i = 0; i < LOCS.length; i++) {
-    		if (parts[2] == LOCS[i] && parts[2] != parts[1]) {
-    			validTo = true;
-    		}
-    	}
-
-    	return (validFrom && validTo);
+        String temp = input;
+        int commaIndex = 0;
+        int previousIndex = 0;
+        int partsIndex = 0;
+        String[] parts = new String[4];
+        boolean validFrom = false;
+        boolean validTo = false;
+        
+        for (int i = 0; i < (temp.length() - 1); i++) {
+            commaIndex = i;
+            if (temp.charAt(i) == ',') {
+                parts[partsIndex] = temp.substring(previousIndex, commaIndex);
+                previousIndex = ++commaIndex;
+                partsIndex++;
+            }
+        }
+        
+        for (int i = 0; i < (LOCS.length - 1); i++) {
+            if (parts[1] == LOCS[i]) {
+                validFrom = true;
+            }
+        }
+        
+        for (int i = 0; i < LOCS.length; i++) {
+            if (parts[2] == LOCS[i] && parts[2] != parts[1]) {
+                validTo = true;
+            }
+        }
+        
+        return (validFrom && validTo);
     }
-
+    
     private boolean isValidPort(int port) {
-    	if (port > 1025 && port < 65535) {
-    		return true;
-    	}
+        if (port > 1025 && port < 65535) {
+            return true;
+        }
 
-    	return false;
+        return false;
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-    	if (args.length == 0) {
-    		System.out.printf("Port not specified. Using free port %d", DEFAULT_PORT);
-    		SafeWalkServer s = new SafeWalkServer();
-    		s.run();
-    	}
-    }		
+        if (args.length == 0) {
+            System.out.printf("Port not specified. Using free port %d", DEFAULT_PORT);
+            SafeWalkServer s = new SafeWalkServer();
+            s.run();
+        } else {
+            try {
+                int port = Integer.parseInt(args[1]);
+                SafeWalkServer s = new SafeWalkServer(port);
+                s.run();
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+    }  
 }
